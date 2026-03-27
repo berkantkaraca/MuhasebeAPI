f39714227935a85e8b6db5bc4eacecfdc7366ed8
Berkant Karaca <berkantkaracatr@gmail.com>
2026-03-28 01:58:33 +0300

    test controller yazıldı

---
+++ Infrastructure/MuhasebeAPI.Presentation/Abstraction/ApiController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MuhasebeAPI.Presentation.Abstraction;
+
+[ApiController]
+[Route("api/[controller]")]
+public abstract class ApiController : ControllerBase
+{
+
+}
---
+++ Infrastructure/MuhasebeAPI.Presentation/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MuhasebeAPI.Presentation.Abstraction;
+
+namespace MuhasebeAPI.Presentation.Controllers;
+
+public sealed class TestController : ApiController
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("İşlem başarılı");
+    }
+}
